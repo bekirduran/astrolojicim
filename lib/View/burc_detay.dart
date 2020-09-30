2a81faece9623b5cf0dcb67cb50fe48b1e9cadf3
Bekir Duran <akinmihal@gmail.com>
2020-09-30 16:38:31 +0300

    3th part lib. implemented (Palette Generator)

--- lib/View/burc_detay.dart
+++ lib/View/burc_detay.dart
@@ -1,19 +1,33 @@
 import 'package:astrolojicim/Modal/burc.dart';
 import 'package:astrolojicim/View/burc_listesi.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetay extends StatelessWidget {
+class BurcDetay extends StatefulWidget {
   int selectedIndex;
-  Burc selectedBurc;
 
   BurcDetay(this.selectedIndex);
 
   @override
+  _BurcDetayState createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+  Burc selectedBurc;
+  Color appBarColor;
+  PaletteGenerator paletteGenerator;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedBurc = BurcListesi.listOfBurc[widget.selectedIndex];
+    colorGenerateFromImage();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final _sizeScreen = MediaQuery.of(context).size;
-    selectedBurc = BurcListesi.listOfBurc[selectedIndex];
 
     return Scaffold(
       body: CustomScrollView(
@@ -23,7 +37,8 @@ class BurcDetay extends StatelessWidget {
             expandedHeight: _sizeScreen.height / 4,
             pinned: true,
             primary: true,
-            backgroundColor: Colors.purple,
+            backgroundColor:
+                appBarColor != null ? appBarColor : Colors.blueGrey.shade400,
             flexibleSpace: FlexibleSpaceBar(
               title: Text(selectedBurc.burcName + " Burcu ve Özellikleri"),
               background: Image.asset(
@@ -32,21 +47,38 @@ class BurcDetay extends StatelessWidget {
               centerTitle: true,
             ),
           ),
-
           SliverToBoxAdapter(
             child: SingleChildScrollView(
-            child: Card(
-              elevation: 5,
-              margin: EdgeInsets.all(20),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(selectedBurc.burcDetail, style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400,),),
+              child: Card(
+                elevation: 5,
+                margin: EdgeInsets.all(20),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    selectedBurc.burcDetail,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
               ),
-            ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void colorGenerateFromImage() {
+    Future<PaletteGenerator> colorGenerator =
+        PaletteGenerator.fromImageProvider(
+            AssetImage("images/" + selectedBurc.burcLargePicture + ".png"));
+    colorGenerator.then((value) {
+      paletteGenerator = value;
+      setState(() {
+        appBarColor = paletteGenerator.dominantColor.color;
+      });
+    });
   }
 }
